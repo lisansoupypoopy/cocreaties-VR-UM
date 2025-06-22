cac7276f11cd135e8107b59e04b0b5d8c05bc179
Lisan Stoffels <144241385+lisansoupypoopy@users.noreply.github.com>
2025-06-22 17:28:02 +0200

    Add files via upload

--- data/cocreatieData.js
+++ data/cocreatieData.js
@@ -2,36 +2,41 @@ export const cocreatieData = [
   {
     person: "Luca",
     tagline: "Lab Skills for Everyone, Anytime",
-    thumbImg: "/VR-lab-thumb.png",
+    thumbImg: {
+      src: "/VR-lab-thumb.png",
+      alt: "Luca practicing in a VR lab"
+    },
+    thumbBg: "#B0AFC6",
+    titleBg: "#7FC29B",
     introduction: {
-        text: (
-            <>
-            Luca and a friend were waiting in the Randwijck canteen, where they had 2 hours to kill before their lecture. 
-            We had cookies, stroopwafels and an opportunity for distraction.
-            <strong> A match made in heaven!</strong>
-            </>
-        ),
-        image: {
-            src: "/image_luca_adia.png",
-            alt: "Luca and Adia in the canteen",
-        },
+      text: (
+        <>
+          Luca and a friend were waiting in the Randwijck canteen, where they had 2 hours to kill before their lecture.
+          We had cookies, stroopwafels and an opportunity for distraction.
+          <strong> A match made in heaven!</strong>
+        </>
+      ),
+      image: {
+        src: "/image_luca_adia.png",
+        alt: "Luca and Adia in the canteen",
+      },
     },
     profile: {
-        name: "Luca",
-        image: {
-            src: "/Luca_profile.svg",
-            alt: "Luca's profile picture"
-        },
-        age: 22,
-        country: "Spain",
-        study: "Biomedical Sciences",
-        personalityTraits: [
+      name: "Luca",
+      image: {
+        src: "/Luca_profile.svg",
+        alt: "Luca's profile picture"
+      },
+      age: 22,
+      country: "Spain",
+      study: "Biomedical Sciences",
+      personalityTraits: [
         { trait: "Creative", color: "#FF63E5" },
         { trait: "Bubbly", color: "#FF63E5" },
         { trait: "Hungry", color: "#B0AFC6" },
         { trait: "Realistic", color: "#B5EF8A" }
-        ],
-        quote: "I am an extrovert that gets along with introverts"
+      ],
+      quote: "I am an extrovert that gets along with introverts"
     },
     brainstorm: [
       {
@@ -57,66 +62,79 @@ export const cocreatieData = [
       }
     ],
     course: {
-        course: "Biomedical Approaches",
-        description: "The course Biomedical Approaches teaches key skills in biomedical sciences, such as research methods, data analysis, and lab techniques."
+      course: "Biomedical Approaches",
+      description: "The course Biomedical Approaches teaches key skills in biomedical sciences, such as research methods, data analysis, and lab techniques."
     },
     courseStory: [
-        {
-            src: "",
-            alt: "A student practicing flow cytometry",
-            text: "One of the skills students learn is Flow cytometry (FC)..."
-        },
-        {
-            src: "",
-            alt: "Crowded lab",
-            text: "That’s why students now often observe experiments..."
-        },
-        {
-            src: "",
-            alt: "Frustrated student",
-            text: "The students were frustrated with certain procedures..."
-        },
-        {
-            src: "",
-            alt: "A student practicing flow cytometry in VR",
-            text: "The solution: A VR experience..."
-        }
+      {
+        src: "/luca_drawing_1.png",
+        alt: "A student practicing flow cytometry",
+        text: "One of the skills students learn is Flow cytometry (FC). This is a technique used to detect and measure the physical and chemical characteristics of a population of cells or particles (e.g. looking for cancer cells). However, this process is very expensive.",
+        textBg: "#FF63E5",
+        imgBg: "#DFFF63"
+      },
+      {
+        src: "/luca_drawing_2.png",
+        alt: "Crowded lab",
+        text: "That’s why students now often observe experiments with larger groups, watch videos about the process, or read stories that explain it instead of performing the procedure themselves.",
+        textBg: "#B0AFC6",
+        imgBg: "#7FC29B"
+      },
+      {
+        src: "/luca_drawing_3.png",
+        alt: "Frustrated student",
+        text: "The students were frustrated with certain procedures they couldn’t try themselves and could only observe, which made the material feel too abstract and difficult to understand. They said that if they could actually try these procedures, they would be able to grasp them more quickly and easily.",
+        textBg: "#FF63E5",
+        imgBg: "#DFFF63"
+      },
+      {
+        src: "/luca_drawing_4.png",
+        alt: "A student practicing flow cytometry in VR",
+        text: "The solution: A VR experience in which the students can choose an experiment to rehearse, so this can be played individually, almost anywhere. This reduces the needed resources so that everyone can try everything.",
+        textBg: "#7FC29B",
+        imgBg: "#B0AFC6"
+      }
     ],
     conclusion: "What if students could actually rehearse complex lab procedures whenever and wherever they want? That’s where virtual rehearsal comes in. Instead of just watching or reading about flow cytometry, they could actually try it, without the expensive equipment or crowded labs. By creating a VR experience where students can explore, repeat, and interact with biomedical experiments, we open the door to contextualized learning. It’s no longer just theory, it’s something they can step into. With focused immersion, distractions fade away, and understanding clicks faster. We wanted to give them extraordinary abilities: zoom into cells, walk through molecular processes, or replay that one tricky step until it makes sense. And because it’s all in VR, students can experience this remote presence from home, the library, or wherever they feel most focused.",
   },
   {
     person: "Adia",
     tagline: "Our mentor is always busy. Why not a virtual one?",
-    thumbImg: "/VR-mentor-thumb.png",
+    thumbImg: {
+      src: "/VR-mentor-thumb.png",
+      alt: "Adia practicing in a VR mentor session"
+    },
+    thumbBg: "#7FC29B",
+    titleBg: "#DFFF63",
     introduction: {
-        text: (
-            <>
-            Adia and a friend were waiting in the Randwijck canteen, where they had 2 hours to kill before their lecture. 
-            We had cookies, stroopwafels and an opportunity for distraction.
-            <strong> A match made in heaven!</strong>
-            </>
-        ),
-        image: {
-            src: "/image_luca_adia.png",
-            alt: "Luca and Adia in the canteen",
-        },
+      text: (
+        <>
+          Adia and a friend were waiting in the Randwijck canteen, where they had 2 hours to kill before their lecture.
+          We had cookies, stroopwafels and an opportunity for distraction.
+          <strong> A match made in heaven!</strong>
+        </>
+      ),
+      image: {
+        src: "/image_luca_adia.png",
+        alt: "Luca and Adia in the canteen",
+      },
     },
     profile: {
-        name: "Adia",
-        image: {
-            src: "/Adia_happy.svg",
-            alt: "Adia's profile picture"
-        },
-        age: 21,
-        country: "Afghanistan",
-        study: "Biomedical Sciences",
-        personalityTraits: [
+      name: "Adia",
+      image: {
+        src: "/Adia_happy.svg",
+        alt: "Adia's profile picture"
+      },
+      age: 21,
+      country: "Afghanistan",
+      study: "Biomedical Sciences",
+      personalityTraits: [
         { trait: "Caring", color: "#FF63E5" },
         { trait: "Extrovert", color: "#B0AFC6" },
         { trait: "Patient", color: "#FF63E5" },
         { trait: "Adventurous", color: "#B5EF8A" }
-        ],
-        quote: "People appreciate my patience and outgoingness.",
+      ],
+      quote: "People appreciate my patience and outgoingness.",
     },
     brainstorm: [
       {
@@ -149,42 +167,55 @@ export const cocreatieData = [
       }
     ],
     course: {
-        course: "Mentoring track",
-        description: "The mentoring track helps students stay on track by setting learning goals and tracking their progress with guidance from a professor or mentor."
+      course: "Mentoring track",
+      description: "The mentoring track helps students stay on track by setting learning goals and tracking their progress with guidance from a professor or mentor."
     },
     courseStory: [
-        {
-            src: "/drawing1.png",
-            alt: "Sisyphos pushing a boulder",
-            text: "This course aims to help students with support from a professor or mentor..."
-        },
-        {
-            src: "/drawing2.png",
-            alt: "Crowded lab",
-            text: ""
-        },
-        {
-            src: "/drawing3.png",
-            alt: "Frustrated student",
-            text: "On the other side there is a professor or tutor that’s very busy..."
-        },
-        {
-            src: "/drawing4.png",
-            alt: "A laptop opened, but no mentor in sight",
-            text: "The solution: A VR experience where the students can choose their own environment..."
-        }
+      {
+        src: "/adia_drawing_1.png",
+        alt: "Sisyphos pushing a boulder",
+        text: "This course aims to help students with support from a professor or mentor. However, many students currently find it difficult and experience the track as unhelpful and frustrating. Like sisyphos pushing a boulder uphill, they struggle to keep up.",
+        textBg: "#FF63E5",
+        imgBg: "#DFFF63"
+      },
+      {
+        src: "/adia_drawing_2.png",
+        alt: "Student struggling to reach the mentor",
+        text: "It’s challenging for students to reach the mentor and successfully arrange a meeting, which can lead to delays in getting feedback or guidance.",
+        textBg: "#B0AFC6",
+        imgBg: "#7FC29B"
+      },
+      {
+        src: "/adia_drawing_3.png",
+        alt: "A laptop opened, but no mentor in sight",
+        text: "On the other side there is a professor or tutor that’s very busy and doesn’t have that much time. This is an extra activity for them to do beside their jobs, resulting in sessions where everything is quickly handled. But sometimes the students wish for more time/help.",
+        textBg: "#FF63E5",
+        imgBg: "#DFFF63"
+      },
+      {
+        src: "/adia_drawing_4.png",
+        alt: "A kayak floating in a calm sea",
+        text: "The solution: A VR experience where the students can choose their own environment, like a relaxing beach or quiet library, and the students can go in on their own accord (or planned sessions) and keep track of their progress in a little booklet (or something else of choice, as long as they can write something on it). The mentor could see the environment as well, and go in on their own accord too or plan sessions with the student.",
+        textBg: "#B0AFC6",
+        imgBg: "#7FC29B"
+      }
     ],
     conclusion: "What if students could reflect on their progress and connect with mentors whenever and wherever they feel most at ease? Instead of rushed meetings squeezed between busy schedules, students can step into a calm, personalized VR space one that invites focus, reflection, and meaningful connection. By creating a VR experience where students can set learning goals, track their progress, and meet with mentors in an environment they choose, we open the door to active autonomy. They decide when to check in, where they want to be, and how they want to work. In this immersive space, distractions fade, and motivation has room to grow. Somewhere they can write in a virtual journal, revisit their goals, or reflect without judgment. And because it’s all in VR, mentors can join too whether in a planned session or spontaneously sharing the same virtual setting. This shared remote presence helps build connection, even from a distance.",
   },
-{
+  {
     person: "Daantje",
     tagline: "Inside the Operating Room: Up Close in VR",
-    thumbImg: "/Vr-operation-thumb.png",
+    thumbImg: {
+      src: "/Vr-operation-thumb.png",
+      alt: "Daantje practicing surgery in VR"
+    },
+    thumbBg: "#B5EF8A",
+    titleBg: "#FF63E5",
     introduction: {
       text: (
         <>
-          Daantje talked about her <em>co-schappen</em>: a hospital internship, which is a period of three years, 
-          where students spend 3 to 6 months in different sectors (like neurology, IC, childcare, etc.) and hospitals at a time. 
+          Daantje talked about her <em>co-schappen</em>: a hospital internship, which is a period of three years,
+          where students spend 3 to 6 months in different sectors (like neurology, IC, childcare, etc.) and hospitals at a time.
           That’s where Medicine shifts from facts in a book to real-life experiences.
         </>
       ),
@@ -246,24 +277,32 @@ export const cocreatieData = [
     },
     courseStory: [
       {
-        src: "/daantje1.png",
+        src: "/daantje_drawing_1.png",
         alt: "A hospital corridor",
-        text: "Medicine becomes reality during internships – students observe, assist, and absorb knowledge in real-world settings.",
+        text: "Of course, it makes perfect sense that students aren’t allowed to just perform operations on their own — that would be incredibly dangerous. They still need proper supervision and years of training before they can safely take on that kind of responsibility.",
+        textBg: "#B5EF8A",
+        imgBg: "#DFFF63"
       },
       {
-        src: "/daantje2.png",
+        src: "/daantje_drawing_2.png",
         alt: "Students watching surgery from a distance",
-        text: "Naturally, students aren't allowed to perform surgeries. They must observe from afar and learn passively.",
+        text: "That’s why students now are kept at a distance during surgeries. ",
+        textBg: "#B0AFC6",
+        imgBg: "#7FC29B"
       },
       {
-        src: "/daantje3.png",
-        alt: "A frustrated student watching surgery",
-        text: "Many feel disconnected – unable to interact or practice, their learning remains theoretical.",
+        src: "/daantje_drawing_3.png",
+        alt: "Seeing surgery in VR",
+        text: "Solution: In virtual reality, students can observe surgeries or even try performing them in a simulated environment. Of course, this isn’t enough to become a surgeon, but it can help build confidence and provide useful knowledge that supports their learning process.",
+        textBg: "#B5EF8A",
+        imgBg: "#FF63E5"
       },
       {
-        src: "/daantje4.png",
-        alt: "VR surgery practice scene",
-        text: "With VR, students can try surgeries in a simulated environment. Failing isn't a risk – it's part of the learning process.",
+        src: "/daantje_drawing_4.png",
+        alt: "VR surgery game",
+        text: "It can also take the form of a game, where students can try again and again, without real-world consequences if they make mistakes. Failing a surgery in VR isn’t a problem it’s part of the learning process. It creates a safe and engaging environment where students can practice, experiment, and build their skills.",
+        textBg: "#B0AFC6",
+        imgBg: "#DFFF63"
       },
     ],
     conclusion:
@@ -272,11 +311,16 @@ export const cocreatieData = [
   {
     person: "Rachel",
     tagline: "Stage Fright Simulator",
-    thumbImg: "/fear-presentation-thumb.png",
+    thumbImg: {
+      src: "/fear-presentation-thumb.png",
+      alt: "Rachel practicing presentation skills in VR"
+    },
+    thumbBg: "#DFFF63",
+    titleBg: "#B0AFC6",
     introduction: {
       text: (
         <>
-          Rachel is a friend of ours. She loves helping students with their research because she knows how difficult it can be to find participants. 
+          Rachel is a friend of ours. She loves helping students with their research because she knows how difficult it can be to find participants.
           She is a PhD student focused on Educational Development Research.
         </>
       ),
@@ -338,24 +382,25 @@ export const cocreatieData = [
     },
     courseStory: [
       {
-        src: "/rachel1.png",
+        src: "/rachel_drawing_1.png",
         alt: "Nervous student presenting",
-        text: "For many students, presentations are nerve-wracking. Nerves take over and content gets lost.",
+        text: "For many students, the thought of giving a presentation is enough to send their heart racing. Standing in front of the class, all eyes on them, they suddenly forget half of what they were going to say. Even with notes in hand, the nerves often take over.",
+        textBg: "#FF63E5",
+        imgBg: "#DFFF63"
       },
       {
-        src: "/rachel2.png",
+        src: "/rachel_drawing_2.png",
         alt: "Student struggling with note cards",
-        text: "Even with preparation, the fear of public speaking can overpower their confidence.",
+        text: "Solution: But what if you could train those skills in a VR world one where you control the chaos? Maybe you start with a serious crowd, but then suddenly everyone’s wearing funny hats, spiders crawl across the floor, or the audience erupts in applause every time you finish a sentence. ",
+        textBg: "#B0AFC6",
+        imgBg: "#7FC29B"
       },
       {
-        src: "/rachel3.png",
+        src: "/rachel_drawing_3.png",
         alt: "Student practicing in VR",
-        text: "In VR, students can choose their audience: silly, scary, supportive—or even empty.",
-      },
-      {
-        src: "/rachel4.png",
-        alt: "Fun VR audience with hats and sound effects",
-        text: "It’s a safe space where failure doesn’t matter and students can rehearse until they feel ready.",
+        text: "Practicing in that kind of environment might just make the real thing feel like a breeze.",
+        textBg: "#FF63E5",
+        imgBg: "#DFFF63"
       },
     ],
     conclusion:
